3b90c06f31a24c15ca79db6fec6f29c0527d8a21
awsam <xyu.wang.0713@gmail.com>
2013-04-02 20:32:04 +0800

    it is ok

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130402092009) do
+ActiveRecord::Schema.define(:version => 20130402122735) do
 
   create_table "checkins", :force => true do |t|
     t.integer  "user_id",                           :null => false
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(:version => 20130402092009) do
     t.integer  "project_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "member_id"
   end
 
   create_table "projects", :force => true do |t|
@@ -42,6 +43,7 @@ ActiveRecord::Schema.define(:version => 20130402092009) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.text     "description"
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
