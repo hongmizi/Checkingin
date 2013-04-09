91ff1909a7c67f67d48a111dac99785d2cc92935
awsam <xyu.wang.0713@gmail.com>
2013-04-09 22:19:30 +0800

    1.optimized code about checkin_controller and projects_controller

--- app/controllers/checkin_controller.rb
+++ app/controllers/checkin_controller.rb
@@ -1,55 +1,58 @@
+# coding: UTF-8
 class CheckinController < ApplicationController
   def create
-  @project = Project.find(params[:project_id])
+    @project = Project.find(params[:project_id])
+  
     @project.checkins.each do |checkin |
-
       time = checkin.created_at
       now = Time.now
       if checkin.user == current_user and time.year == now.year and time.month == now.month and  time.day == now.day
-        flash.alert = "you already checked in !!!"
+        flash.alert = "你已经签到过了!"
         redirect_to project_path(params[:project_id])
-        return 
+        return
       end
     end
-     c = Checkin.new(:user_id => current_user.id,:project_id => @project.id, :state => "pending")
-     if c.save
-       Notifier.check_in(current_user,@project,c).deliver
-       flash.alert = "success added check in!"
+
+     checkin =  Checkin.new(:user_id => current_user.id,:project_id => @project.id)
+     if checkin.save
+       Notifier.check_in(current_user,@project,checkin).deliver
+       flash.notice = "恭喜你成功签到!" 
      else
-        flash.alert = "failed added check in!!"
+       flash.alert = "签到失败!"
      end
     redirect_to project_path(params[:project_id])
     return 
-
   end
 
   def update
-    checkin = params[:id]
-    state = params[:state]
+    id         = params[:id]
+    state      = params[:state]
     project_id = params[:project_id]
+    
     project = Project.find(project_id)
     #判断是否是项目管理人
     @admin = project.owner
     if @admin.blank? or current_user.blank? or  @admin.id != current_user.id
-      flash.alert = 'you is not admin !'
+      flash.alert = '对不起,你不是项目管理人!'
       redirect_to project_path(project_id)
       return 
     end
-    c= project.checkins.find(checkin)
-    if c.state != "pending"
-      flash.alert = 'you already declined/approved !'
+
+    checkin = project.checkins.find(id)
+    if checkin.state != "pending"
+      flash.alert = '你已经审批过了!'
       redirect_to project_path(project_id)
       return 
     end
     if state == 'approved'
-      c.approve
+      checkin.approve
     elsif state == 'declined'
-      c.decline
+      checkin.decline
     end
-    if c.save
-      flash.notice = 'success'
+    if checkin.save
+      flash.notice = '审批成功!'
     else
-      flash.alert = 'failed!'
+      flash.alert = '审批失败!'
     end
     redirect_to project_path(project_id)
   end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,3 +1,4 @@
+# coding: UTF-8
 class ProjectsController < ApplicationController
   def index
   end
@@ -5,16 +6,12 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
     @new_member = ""
-    if current_user.projects.include?(@project)
-      @admin = true
-    else
-      @admin = false
-    end
+    @admin = true if current_user == @project.owner
+
     # 统计数量
-    @approve = {}
-    @decline = {}
-    @pending = {}
-    @sum = {}
+    @approve, @decline, @pending, @sum = {}, {}, {},{}
+
+    #initialize
     @project.users.each do |user|
       @approve[user] = 0
       @decline[user] = 0
@@ -22,20 +19,13 @@ class ProjectsController < ApplicationController
       @pending[user] = 0
     end
 
-    @project.checkins.each do |checkin |
-      if checkin.state == "approved"
-        @approve[checkin.user] += 1
-      end
-      if checkin.state == "declined"
-        @decline[checkin.user] += 1
-      end
-      if checkin.state == "pending"
-        @pending[checkin.user] += 1
-      end
+    if @project.checkins.each do |checkin |
+      @approve[checkin.user] += 1 if checkin.state == "approved"
+      @decline[checkin.user] += 1 if checkin.state == "declined"
+      @pending[checkin.user] += 1 if checkin.state == "pending"
       @sum[checkin.user] += 1
-
     end
-
+    end
   end
 
   def new
@@ -44,7 +34,6 @@ class ProjectsController < ApplicationController
 
   def create
     @project = current_user.projects.new(params[:project])
-
     if @project.save
       redirect_to user_path(current_user.id)
     else
@@ -59,33 +48,38 @@ class ProjectsController < ApplicationController
   end
 
   def update
-    #get => push   身份验证
     id = params[:id]
     @project = Project.find(id)
     if not @project
-      flash.alert = "can't find Project Plase try again!!!"
+      flash.alert = "不能找到此项目,请重试!"
       redirect_to project_path(id)
       return 
+    end
+    if @project.owner != current_user
+      flash.alert = "你不是此项目的管理员!"
+      redirect_to project_path(id)
+      return
     end
     user_email= params[:new_member]
     @user = User.find_by_email(user_email)
     if not @user
-      flash.alert = "can't find user!!!"
+      flash.alert = "不能找到相应用户,请重新输入!"
       redirect_to project_path(id)
       return
     end
     if @project.users.include?(@user)
-      flash.alert = "already have this user!"
+      flash.alert = "此用户已经在项目中了.."
       redirect_to project_path(id)
       return
     end
-     a= Membership.new :user_id => @user.id, :project_id => @project.id
-    if a.save
-      flash.notice = 'success'
-    else
-      flash.notice = 'added failed'
-    end
+    if Membership.create!(:user_id => @user.id, :project_id => @project.id)
+      flash.notice = "成功添加用户!"
       redirect_to project_path(id)
       return 
+    else
+      flash.alert = "添加用户失败!"
+      redirect_to project_path(id)
+      return
+    end
   end
 end
