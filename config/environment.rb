e182e2ea0d2ae0d8dcce3d7b84ab44b7fbc96279
awsam <xyu.wang.0713@gmail.com>
2013-04-10 16:59:10 +0800

    email for heroku

--- config/environment.rb
+++ config/environment.rb
@@ -5,14 +5,14 @@ require File.expand_path('../application', __FILE__)
 Checkingin::Application.initialize!
 Checkingin::Application.configure do
   config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.smtp_settings = {
+end
+ActionMailer::Base.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
     :domain => "domain.of.sender.net",
     :authentication => "plain",
     :user_name => "checkinginathmz",
     :password => "hongmizi",
-    :enable_starttls_auto => true
-  }
-end
+    :enable_starttls_auto => true,
+    :authentication  => :login
+}
