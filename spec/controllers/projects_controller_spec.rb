47119c9c5d42adb69249525a3d49afa2130b731d
awsam <xyu.wang.0713@gmail.com>
2013-04-10 12:29:42 +0800

    add test for projectController

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -2,7 +2,10 @@ require 'spec_helper'
 describe ProjectsController do
   before do
     @request.env["devise.mapping"] = Devise.mappings[:user]
-    sign_in FactoryGirl.create(:user) 
+    
+    @user = FactoryGirl.create(:user) 
+    @project = FactoryGirl.create(:project)
+    sign_in @user
   end
   describe "Test routes" do
     it " get new should have respond " do
@@ -11,9 +14,19 @@ describe ProjectsController do
       response.code.should == "200"
     end
     it "show should have respond" do
-      get "new" 
+      get :show, id:@project.id
       response.should be_success
       response.code.should == "200"
+    end
+    it "create should have respond" do
+      post :create, name:@project.name, description:@project.description
+      response.should be_success
+      response.code.should == "200"
+    end
+    it "update should have respond" do
+      put :update, id:@project, new_member:@user.email
+     # response.should be_success
+      response.code.should == "302"
     end
   end
 end
