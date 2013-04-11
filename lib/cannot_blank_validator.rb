d61460ae3bb4f3579d3c2afe86c34dc59f7bbe0e
awsam <xyu.wang.0713@gmail.com>
2013-04-11 12:50:19 +0800

    delete 2 file

--- lib/cannot_blank_validator.rb
+++
@@ -1,7 +0,0 @@
-class Cannot_Blank < ActiveModel::EachValidator
-    def validate_each(object, attribute, value)
-          if  value == nil or value == ""
-            object.errors.add(attribute, :cannot_blank, options)
-          end
-    end
-end
