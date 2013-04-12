0885e98f9962cdc55e51710522f3151df13e85ae
awsam <xyu.wang.0713@gmail.com>
2013-04-13 00:39:30 +0800

    show logs in development env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,8 @@
 Checkingin::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
+  config.logger = Logger.new(STDOUT)
+  config.logger.level = Logger.const_get(ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].upcase : 'DEBUG' )
+
+ #Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
@@ -35,5 +38,4 @@ Checkingin::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 end
-
 require 'ruby-debug'
