2ce29d394626d7ef4c5471b3078d08356bcbfc27
awsam <xyu.wang.0713@gmail.com>
2013-06-19 21:05:36 +0800

    fix the bug of management#index

--- app/domain/checkin_domain.rb
+++ app/domain/checkin_domain.rb
@@ -16,6 +16,28 @@ class CheckinDomain
       checkins_on_month
   end
 
+  def get_user_checkins_on_month(time, user_id, project_id)
+    @time = time
+    @project = Project.find(project_id)
+    @user = User.find(user_id)
+    begin
+      @checkins_on_month = {}
+      for day in 1..Time.days_in_month(@time.month, @time.year)
+        @checkins_on_month[day] = nil
+      end
+      @user.checkins.where(:project_id => @project.id).each do |checkin|
+        time = checkin.created_at
+        if time.month == @time.month and time.year == @time.year
+          @checkins_on_month[time.day] = checkin
+        end
+      end
+    rescue Exception
+    end
+    return @checkins_on_month
+  end
+
+
+
   def self.state_of_checkins checkins_on_month
     pending = 0
     declined = 0
